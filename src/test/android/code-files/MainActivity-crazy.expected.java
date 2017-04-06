0d89c4ba4c2649ecf3aa1537ccea5e4767c80051
foreverest <dmitry.zabello@gmail.com>
2017-04-06 17:49:58 +0400

    Crazy code test

---
+++ src/test/android/code-files/MainActivity-crazy.expected.java
@@ -0,0 +1,37 @@
+package com.example.foreverest.helloandroid;
+
+    import
+            
+        android.support.v7.app.AppCompatActivity;
+                
+                            import
+        
+        
+        android.os.Bundle;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+import com.microsoft.azure.mobile.distribute.Distribute;
+
+public
+class
+MainActivity
+        extends             AppCompatActivity
+
+
+{
+
+    @Override
+    protected
+    void
+    onCreate
+            (
+                    Bundle savedInstanceState   )
+
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileCenter.start(getApplication(), "00000000-0000-0000-0000-000000000000",
+                Analytics.class, Crashes.class, Distribute.class);
+    }
+}
